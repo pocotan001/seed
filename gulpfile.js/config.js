7b7054bc06eb7a0bd8f3aa16ba4173d79d132835
pocotan001 <pocotan001@gmail.com>
2015-04-21 14:21:06 +0900

    Remove process.env.NODE_ENV

--- gulpfile.js/config.js
+++ gulpfile.js/config.js
@@ -6,6 +6,6 @@ var argv = require('minimist')(process.argv.slice(2));
 module.exports = {
     name: pkg.name.replace('-', ''),
     dirs: pkg.directories,
-    env: process.env.NODE_ENV || (argv.p || argv.production ? 'production' : 'development'),
+    env: (argv.p || argv.production) ? 'production' : 'development',
     watch: !!(argv.w || argv.watch)
 };
