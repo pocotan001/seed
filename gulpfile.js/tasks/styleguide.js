d4f7c5b3ced6be1c33d3e19a14d02735623970c8
pocotan001 <pocotan001@gmail.com>
2015-04-28 17:26:32 +0900

    :lipstick:

--- gulpfile.js/tasks/styleguide.js
+++ gulpfile.js/tasks/styleguide.js
@@ -12,7 +12,7 @@ gulp.task('styleguide', function() {
         watched = true;
     }
 
-    gulp.start(['styleguide:generate', 'styleguide:app-css', 'styleguide:sg-css']);
+    gulp.start(['styleguide:generate', 'styleguide:applystyles', 'styleguide:copy']);
 });
 
 gulp.task('styleguide:generate', function() {
@@ -20,23 +20,25 @@ gulp.task('styleguide:generate', function() {
         .pipe(styleguide.generate({
             title: 'Seed Style Guide',
             overviewPath: 'README.md',
-            extraHead: '<link rel="stylesheet" href="sg.css"><script src="/app.js"></script>',
+            extraHead: '<link rel="stylesheet" href="/sg.css"><script src="/app.js"></script>',
             commonClass: 'sg-common',
             rootPath: '.',
-            appRoot: '/styleguide',
             disableEncapsulation: true,
             disableHtml5Mode: true
         }))
         .pipe(gulp.dest(config.dirs.styleguide));
 });
 
-gulp.task('styleguide:app-css', function() {
+gulp.task('styleguide:applystyles', function() {
     return gulp.src(config.dirs.dest + '/app.css')
         .pipe(styleguide.applyStyles())
         .pipe(gulp.dest(config.dirs.styleguide));
 });
 
-gulp.task('styleguide:sg-css', function() {
-    return gulp.src('gulpfile.js/fixtures/styleguide/sg.css')
+gulp.task('styleguide:copy', function() {
+    return gulp.src([
+        'gulpfile.js/fixtures/styleguide/sg.css',
+        config.dirs.dest + '/app.js'
+    ])
         .pipe(gulp.dest(config.dirs.styleguide));
 });
