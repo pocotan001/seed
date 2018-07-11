c3074ce40e94777a8f71fca20b785a14dc65f967
pocotan001 <pocotan001@gmail.com>
2018-07-11 15:40:26 +0900

    updates

--- src/server/middleware/api/csrf.ts
+++ src/server/middleware/api/csrf.ts
@@ -3,14 +3,14 @@ import { RequestHandler } from "express-serve-static-core";
 const IGNORE_METHODS = ["GET", "HEAD", "OPTIONS"];
 
 const csrf = (): RequestHandler => (req, _, next) => {
-  if (!req.xhr || !IGNORE_METHODS.includes(req.method)) {
-    const err = new Error("CSRF validation failed");
-    err.status = 403;
-
-    return next(err);
+  if (req.xhr || IGNORE_METHODS.includes(req.method)) {
+    return next();
   }
 
-  next();
+  const err = new Error("CSRF validation failed");
+  err.status = 403;
+
+  next(err);
 };
 
 export default csrf;
