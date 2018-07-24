9df961c77cb5f83266566fc9a6182b8ff322866d
pocotan001 <pocotan001@gmail.com>
2018-07-24 15:27:40 +0900

    ⬆️

--- src/typings/express.d.ts
+++ src/typings/express.d.ts
@@ -3,6 +3,7 @@ import { RootService } from "~/server/service";
 declare global {
   namespace Express {
     export interface Request {
+      session: CookieSessionInterfaces.CookieSessionObject;
       ctx: {
         service?: RootService;
       };
