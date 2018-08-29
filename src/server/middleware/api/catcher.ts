39490e420d70d9b94470df94869adc58b7e0223d
pocotan001 <pocotan001@gmail.com>
2018-08-30 02:22:23 +0900

    update

--- src/server/middleware/api/catcher.ts
+++ src/server/middleware/api/catcher.ts
@@ -10,13 +10,13 @@ interface IApiErrorResponse {
 const log = createLogger("[api]");
 
 const catcher = (): ErrorRequestHandler => (err: Error, _, res, __) => {
+  log.error(err.stack);
   err = normalizeError(err);
 
   const resp: IApiErrorResponse = {
     error: pick(err, ["message", "code", "data"])
   };
 
-  log.error(err.stack);
   res.status(err.status!).json(resp);
 };
 
