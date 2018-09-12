64e65e11bd5e9c42ce6452b99812c78409f28d97
pocotan001 <pocotan001@gmail.com>
2018-09-12 16:27:28 +0900

    updates

--- src/routes/onRouteError.tsx
+++ src/routes/onRouteError.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import ErrorPage from "~/components/pages/ErrorPage";
+import { createTitle } from "~/domain/Document";
 import { normalizeError } from "~/domain/Error";
 import { RouteErrorAction } from "~/infra/router";
 
@@ -13,7 +14,7 @@ const onRouteError: RouteErrorAction = err => {
   return {
     status: err.status,
     component: <ErrorPage error={err} />,
-    title: err.message,
+    title: createTitle(err.message),
     meta: [{ name: "robots", content: "noindex,nofollow" }]
   };
 };
